47d9764fb2dbe32ec59722849e42cfe74eebca52
duckydee <n.robson91@gmail.com>
2015-10-30 21:13:58 -0400

    Stars! Not optimal..

--- public/src/system/RenderSystem.js
+++ public/src/system/RenderSystem.js
@@ -13,12 +13,20 @@ RenderSystem.prototype.init = function(camera)
     this.context = this.canvas.getContext("2d");
     this.context.font = "32px sans bold";
     this.context.textBaseline = "top";
+
+    this.stars = [];
+    var c = 4000;
+    var d = 2000;
+    for (var i = 0; i < c; i ++) {
+        this.stars.push(Math.floor(Math.random()*d) - d * 0.5);
+        this.stars.push(Math.floor(Math.random()*d) - d * 0.5);
+        this.stars.push(Math.random());
+    }
 }
 
 RenderSystem.prototype.update = function()
 {
-    this.context.fillStyle = "rgb(60,160,80)";
-    this.context.fillRect(0, 0, 800, 600);
+    this.drawBackground();
 
     this.context.fillStyle = "rgb(255,0,0)";
 
@@ -36,5 +44,20 @@ RenderSystem.prototype.addEntity = function(entity)
 {
     if (entity.render) {
         this.entities.push(entity);
+    }
+}
+RenderSystem.prototype.drawBackground = function()
+{
+    this.context.fillStyle = "rgb(0,0,0)";
+    this.context.fillRect(0, 0, 800, 600);
+
+    this.context.fillStyle = "rgb(200,200,200)";
+    for (var i = 0; i < this.stars.length; i += 3) {
+        this.context.fillRect(
+            (this.stars[i] - this.camera.x * 0.25) * this.stars[i+2] * this.camera.zoom + this.canvas.width * 0.5,
+            (this.stars[i+1] - this.camera.y * 0.25) * this.stars[i+2] * this.camera.zoom + this.canvas.height * 0.5,
+            this.stars[i+2] * this.camera.zoom,
+            this.stars[i+2] * this.camera.zoom
+        );
     }
 }
